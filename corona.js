7cde0d4cd1eb88d6b7babd01d53adb3283719b62
Gaurav Hagone <142501350+Gauravbhau@users.noreply.github.com>
2024-01-16 13:42:16 +0530

    Add files via upload

---
+++ corona.js
@@ -0,0 +1,25 @@
+gsap.to("#nav", {
+    backgroundColor: "black",
+    duration:0.5,
+    height: "80px",
+    scrollTrigger: {
+      trigger: "#nav",
+      scroller: "body",
+      // markers:true,
+      start: "top -10%",
+      end: "top -11%",
+      scrub: 1,
+    },
+  });
+  
+  gsap.to("#main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+      trigger: "#main",
+      scroller: "body",
+      // markers: true,
+      start: "top -25%",
+      end: "top -70%",
+      scrub: 5,
+    },
+  });
